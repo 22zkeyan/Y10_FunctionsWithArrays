666db035b73616910649abe291534c01b03b9f27
22zkeyan <22ZKeyan@heckgrammar.co.uk>
2025-11-04 13:20:18 +0000

    Add project files.

---
+++ Y10_FunctionsWithArrays/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Y10_FunctionsWithArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
