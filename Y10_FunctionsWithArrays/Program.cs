380bd5f897f255ce0fc998144412aac2cc19d044
22zkeyan <22ZKeyan@heckgrammar.co.uk>
2025-11-04 14:11:23 +0000

    add2dnums

--- Y10_FunctionsWithArrays/Program.cs
+++ Y10_FunctionsWithArrays/Program.cs
@@ -16,15 +16,17 @@
                 Console.WriteLine(nums[i]);
             }
         }
-        static void List2DNums(int[,] nums) //parameter for names as a 1d array
+        static int Add2DNums(int[,] nums) //parameter for names as a 1d array
         {
+            int total = 0;
             for (int i = 0; i < nums.GetLength(0); i++) //.GetLength(0) tells you how many arrays there are in the 2d array
             {
                 for (int j = 0; j < nums.GetLength(1); j++) //GetLength(1) tells you many elements there are in one array
                 {
-                    Console.WriteLine(nums[i, j]);
+                    total += nums[i, j];
                 }
             }
+            return total;
         }
         static void ListNamesWithBreaks(string[,] names)
         {
@@ -60,7 +62,8 @@
             string[,] twoDnames = { { "Dave", "Tim", "Lucy" }, { "Jane", "Steve", "Alan" }, { "Julie", "Amy", "Jim" } };
             ListNamesWithBreaks(twoDnames);
             int[,] twoDnums = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
-            List2DNums(twoDnums);
+            int total = Add2DNums(twoDnums);
+            Console.WriteLine(total);
         }
     }
 }
