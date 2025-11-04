5e96e5c7ce017959d52226829258c255a5d26bfa
22zkeyan <22ZKeyan@heckgrammar.co.uk>
2025-11-04 13:31:18 +0000

    func tions

--- Y10_FunctionsWithArrays/Program.cs
+++ Y10_FunctionsWithArrays/Program.cs
@@ -2,9 +2,31 @@
 {
     internal class Program
     {
+        static void List(string[] names) //parameter for names as a 1d array
+        {
+            for (int i = 0; i < names.GetLength(0); i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+        }
+        static int[] numbers(int a) //returns an array of integers
+        {
+            int[] nums = new int[5];
+            for (int i = 1; i <= nums.Length; i++)
+            {
+                nums[i-1] = a * i;
+            }
+            return nums;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] names = { "Bartholomew", "Ebeneezer", "Pneumonoultramicroscopicsilicovolcanoconiosis", "William", "Cratchit" };
+            List(names);
+            int[] times_table = numbers(5);
+            foreach (int i in times_table)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
